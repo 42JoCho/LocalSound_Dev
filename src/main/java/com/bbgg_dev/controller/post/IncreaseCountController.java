e4db606e489d670df0331558fcd1aa8c171aae20
mingiSeo <smki001@naver.com>
2021-07-30 01:07:57 +0900

    추천수, 조회수 Controller 수정, 단순 조회 Controller (LookUpPostController) 추가

--- src/main/java/com/bbgg_dev/controller/post/IncreaseCountController.java
+++ src/main/java/com/bbgg_dev/controller/post/IncreaseCountController.java
@@ -4,6 +4,7 @@ import com.bbgg_dev.post.Impl.PostDAO;
 import com.bbgg_dev.post.PostVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,22 +12,28 @@ import javax.servlet.http.HttpServletRequest;
 public class IncreaseCountController {
 
     @RequestMapping(value = "/IncreaseViewCount.do")
-    public void IncreaseViewCount(HttpServletRequest request){
+    public ModelAndView IncreaseViewCount(HttpServletRequest request){
         PostVO vo = new PostVO();
         PostDAO postDAO = new PostDAO();
 
         int pid = Integer.parseInt(request.getParameter("pid"));
         vo.setPostId(pid);
         postDAO.updateViewCount(vo);
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("/detail.jsp");
+        return mav;
     }
 
     @RequestMapping(value = "/IncreaseRecoCount.do")
-    public void IncreaseRecoCount(HttpServletRequest request){
+    public ModelAndView IncreaseRecoCount(HttpServletRequest request){
         PostVO vo = new PostVO();
         PostDAO postDAO = new PostDAO();
 
         int pid = Integer.parseInt(request.getParameter("pid"));
         vo.setPostId(pid);
         postDAO.updateRecoCount(vo);
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("/detail.jsp");
+        return mav;
     }
 }
