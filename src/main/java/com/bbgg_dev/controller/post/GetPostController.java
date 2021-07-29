ce74dcf698482676b0c856670704be7eb9e1438c
mingiSeo <smki001@naver.com>
2021-07-29 15:18:59 +0900

    commentList 오류 수정

--- src/main/java/com/bbgg_dev/controller/post/GetPostController.java
+++ src/main/java/com/bbgg_dev/controller/post/GetPostController.java
@@ -1,5 +1,7 @@
 package com.bbgg_dev.controller.post;
 
+import com.bbgg_dev.comment.CommentVO;
+import com.bbgg_dev.comment.Impl.CommentDAO;
 import com.bbgg_dev.post.Impl.PostDAO;
 import com.bbgg_dev.post.PostVO;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 
 @Controller
@@ -28,14 +31,20 @@ public class GetPostController {
         HttpSession session = request.getSession();
         session.setAttribute("pid", vo.getPostId());
 
+        CommentDAO commentDAO = new CommentDAO();
+        CommentVO commentVO = new CommentVO();
+
         PostDAO postDAO = new PostDAO();
         PostVO post = postDAO.getPost(vo);
+        commentVO.setPostId(Integer.parseInt((String) session.getAttribute("pid")));
+        List<CommentVO> commentList = commentDAO.getCommentList(commentVO);
 
         // 3. 결과를 세션에 저장하고 상세 화면 리턴
         ModelAndView mav = new ModelAndView();
+        mav.addObject("commentList", commentList);
         mav.addObject("post", post);
         mav.setViewName("/detail.jsp");
-        //
+
         return mav;
     }
 }
